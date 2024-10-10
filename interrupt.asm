55d8a538ab8d646b2c30c8daba00854f24b87d33
franciscoctavares <francisco.ctavares@protonmail.com>
2024-10-10 09:16:06 +0100

    aula de quinta

--- interrupt.asm
+++ interrupt.asm
@@ -1,7 +1,20 @@
 # you must put here the necessary code to deal with the interrupts
-
 .data
-enable_interrupts:
+
+ALL_INT_MASK: .word 0x0000ff00
+TIMER_INT_MASK: .word 0x00008000
+
+.text
+int_enable:
 	mfc0 $t0, $12
+	
+	lw $t1, ALL_INT_MASK
+	not $t1,$t1
+	and $t0,$t0,$t1 # disable all interrupts
+	
+	lw $t1, TIMER_INT_MASK
+	or $t0,$t0,$t1 # activate timer interrupt
+	
+	mtc0 $t0, $12
+	
 	jr $ra
-	li $v0, 5
--- main.asm
+++ main.asm
@@ -1,37 +1,38 @@
 #this is the entry point of the program
-	.data
+.data
 STRING_done: .asciiz "Multitask started\n"
 STRING_main: .asciiz "Task Zero\n"
-PCB_BLOCKS: .space 1360 # 136 bytes x 10 Tasks
-MAIN_TASK_PCB: .space 136 # for 32 register + epc
-RUNNING_LIST: .word 0
-READY_LIST: .word 0
-NEXT_PCB: .word 136
-NEXT_POINTER: .word 132
 
-#.eqv NEXT_PCB 136
-.eqv PCB_NEXT_POINTER 132 # pointer to next pcb block
-	.text
+PCB_BLOCKS: .space 1320 # 132 bytes x 10 Tasks
+MAIN_TASK_PCB: .space 132 # for 32 register + epc
+
+RUNNING_LIST: .word 0x00000000
+READY_LIST: .word 0x00000000
+LAST_READY: .word 0x00000000
+AVAILABLE_LIST: .word 0x00000000
+
+.eqv PCB_SIZE 136
+
+.text
 main:
 # prepare the structures
-	li $v0, 1
-	li $a0, 10
-	syscall
 	jal prep_multi
+	li $v0, 10
+	syscall
 	
 # newtask (t0)
-	la $a0,t0
+	#la $a0, $t0
 	li $a1, 1
 	jal newtask
 	li $v0, 10
 	syscall
 	
 # newtask(t1)	
-	la $a0,t1
+	#la $a0,t1
 	li $a1,2
 	jal newtask
 # newtask(t2)
-	la $a0,t2
+	#la $a0,t2
 	li $a2, 3
 	jal newtask
 
@@ -53,23 +54,21 @@ infinit:
 # the support functions	
 prep_multi:
 	la $t0, PCB_BLOCKS
-	li $t1, 0
+	sw $t0, AVAILABLE_LIST
 	
-	loop:
-		li $v0, 1
-		li $a0, 10
-		syscall
-		
-		la $t2, NEXT_PCB($t0)
-		sw $t2, 10($t0)
-		addi $t0, $t0, 136
-		addi $t1, $t1, 1
-		ble $t1, 2, loop
-		
-		la $s0, RUNNING_LIST
-		la $s1, READY_LIST
+	la $s0, RUNNING_LIST
+	la $s1, READY_LIST
+	la $s2, LAST_READY
+
+	addi $sp, $sp, -4
+	sw $ra, 0($sp)
 	
-		jr $ra
+	jal int_enable
+	
+	lw $ra, 0($sp)
+	addi $sp, $sp, 4
+	
+	jr $ra
 	
 newtask:
 	# write your code here
@@ -79,8 +78,9 @@ start_multi:
 	# write your code here
 	jr $ra 
 
-	.include "t0.asm"
-	.include "t1.asm"
-	.include "t2.asm"
-	.include "interrupt.asm"
+.include "interrupt.asm"
+.include "t0.asm"
+.include "t1.asm"
+.include "t2.asm"
+
 #END
