c164a8bd992331a59be435572f05e930bffd9bac
franciscoctavares <francisco.ctavares@protonmail.com>
2024-10-11 18:01:19 +0100

    bug
    falta corrigir o bug do epc(ou ra, ainda não sei) e ver se ele troca bem para as novas tasks.

--- interrupt.asm
+++ interrupt.asm
@@ -1,12 +1,17 @@
 # you must put here the necessary code to deal with the interrupts
 .data
 
+PCB_BLOCKS: .space 1320 # 132 bytes x 10 Tasks
+
 ALL_INT_MASK: .word 0x0000ff00
 KBD_INT_MASK: .word 0x00010000
 
 RCR: .word 0xffff0000
 
-TEST_STRING: .asciiz "Writting stuff from the timer interrupt!"
+TEST_STRING: .asciiz "Writting stuff from the timer interrupt!\n"
+
+.eqv PCB1 132
+.eqv PCB3 396
 
 .text
 int_enable:
@@ -51,7 +56,35 @@ non_int:
 	b int_end
 
 timer_int:
-	la $a0, TEST_STRING
+	#la $a0, TEST_STRING
+	move $a0, $s4
+	jal save_registers
+	
+	beq $s4, 0, skip_loading
+	
+	li $a0, 0
+	jal load_registers
+	
+	skip_loading:
+	jal next_process # v0 = process id of next task
+	
+	li $a0, 0
+	jal save_registers
+	
+	move $a0, $v0
+	jal load_registers
+	
+	b int_end
+	
+	
+
+# gravar registos do processo em execução
+# carregar registos do processo principal
+# atualizar registos e ponteiros para o próximo processo
+# gravar registos do processo principal
+# carregar registos do próximo processo
+# sair da interrupção
+
 
 int_end:
 	lw $v0 , save_v0
@@ -63,3 +96,153 @@ int_end:
 	ori $k0 , 0x0001
 	mtc0 $k0 , $12
 	eret
+	
+# a0 = PCB offset
+save_registers:
+	addi $sp, $sp, -8
+	sw $t0, 0($sp)
+	sw $t1, 4($sp)
+
+	mulu $t0, $a0, PCB_SIZE
+	
+	la $t1, PCB_BLOCKS
+	add $t1, $t1, $t0
+	
+	sw $at, 0($t1)
+	sw $v0, 4($t1)
+	sw $v1, 8($t1)
+	sw $a0, 12($t1)
+	sw $a1, 16($t1)
+	sw $a2, 20($t1)
+	sw $a3, 24($t1)
+	#sw $t0,
+	#sw $t1,
+	sw $t2, 36($t1)
+	sw $t3, 40($t1)
+	sw $t4, 44($t1)
+	sw $t5, 48($t1)
+	sw $t6, 52($t1)
+	sw $t7, 56($t1)
+	sw $s0, 60($t1)
+	sw $s1, 64($t1)
+	sw $s2, 68($t1)
+	sw $s3, 72($t1)
+	sw $s4, 76($t1)
+	sw $s5, 80($t1)
+	sw $s6, 84($t1)
+	sw $s7, 88($t1)
+	sw $t8, 92($t1)
+	sw $t9, 96($t1)
+	sw $k0, 100($t1)
+	sw $k1, 104($t1)
+	sw $gp, 108($t1)
+	sw $sp, 112($t1)
+	sw $fp, 116($t1)
+	sw $ra, 120($t1)
+	
+	mfc0 $t2, $14
+	sw $t2, 124($t1)
+	
+	lw $t1, 4($sp)
+	lw $t0, 0($sp)
+	addi $sp, $sp, 8
+	
+	la $t2, PCB_BLOCKS
+	
+	sw $t0, 28($t2)
+	sw $t1, 32($t2)
+		
+	jr $ra
+	
+# a0 = PCB offset
+load_registers:
+	addi $sp, $sp, -12
+	sw $t0, 0($sp)
+	sw $t1, 4($sp)
+	sw $ra, 8($sp)
+
+	mulu $t0, $a0, PCB_SIZE
+	
+	la $t1, PCB_BLOCKS
+	add $t1, $t1, $t0
+	
+	lw $at, 0($t1)
+	lw $v0, 4($t1)
+	lw $v1, 8($t1)
+	lw $a0, 12($t1)
+	lw $a1, 16($t1)
+	lw $a2, 20($t1)
+	lw $a3, 24($t1)
+	#sw $t0,
+	#sw $t1,
+	lw $t2, 36($t1)
+	lw $t3, 40($t1)
+	lw $t4, 44($t1)
+	lw $t5, 48($t1)
+	lw $t6, 52($t1)
+	lw $t7, 56($t1)
+	lw $s0, 60($t1)
+	lw $s1, 64($t1)
+	lw $s2, 68($t1)
+	lw $s3, 72($t1)
+	lw $s4, 76($t1)
+	lw $s5, 80($t1)
+	lw $s6, 84($t1)
+	lw $s7, 88($t1)
+	lw $t8, 92($t1)
+	lw $t9, 96($t1)
+	lw $k0, 100($t1)
+	lw $k1, 104($t1)
+	lw $gp, 108($t1)
+	lw $sp, 112($t1)
+	lw $fp, 116($t1)
+	lw $ra, 120($t1)
+	
+	#mfc0 $t2, $14
+	#sw $t2, 124($t1)
+	
+	lw $t2, 124($t1)
+	mtc0 $t2, $14
+	
+	lw $ra, 8($sp)
+	lw $t1, 4($sp)
+	lw $t0, 0($sp)
+	addi $sp, $sp, 12
+	
+	la $t2, PCB_BLOCKS
+	
+	lw $t0, 28($t2)
+	lw $t1, 32($t2)
+		
+	jr $ra
+	
+next_process:
+	la $t0, PCB_BLOCKS
+	addi $t0, $t0, PCB3
+	
+	beq $s0, $t0, last_task_running
+	addi $s0, $s0, PCB1
+	continue_ready:
+	beq $s1, $t0, last_task_ready
+	addi $s1, $s1, PCB1
+	continue_lastready:
+	beq $s2, $t0, last_task_lastready
+	addi $s2, $s2, PCB1
+	
+	b done_switching
+	
+	last_task_running:
+		addi $s0, $s0, -396 # -1 * PCB3
+		b continue_ready
+	last_task_ready:
+		addi $s1, $s1, -396 # -1 * PCB3
+		b continue_lastready
+	last_task_lastready:
+		addi $s2, $s2, -396 # -1 * PCB3
+	done_switching:
+		move $t0, $s0
+		addi $t0, $t0, 76
+		
+		lw $v0, 0($t0) # next process id, to be returned from function
+		
+		jr $ra
--- main.asm
+++ main.asm
@@ -1,45 +1,53 @@
 #this is the entry point of the program
+
 .data
 STRING_done: .asciiz "Multitask started\n"
 STRING_main: .asciiz "Task Zero\n"
 
-PCB_BLOCKS: .space 1320 # 132 bytes x 10 Tasks
-MAIN_TASK_PCB: .space 132 # for 32 register + epc
+string_test_error: .asciiz "Pointers with wrong values!\n"
+err1: .asciiz "ready pointer wrong!\n"
+err2: .asciiz "last ready pointer wrong!\n"
+err3: .asciiz "running pointer wrong!\n"
+success_testing: .asciiz ""
+
+test_string: .asciiz "Preparation done!\n"
+
+
 
 RUNNING_LIST: .word 0x00000000
 READY_LIST: .word 0x00000000
 LAST_READY: .word 0x00000000
 AVAILABLE_LIST: .word 0x00000000
 
-.eqv PCB_SIZE 136
+.eqv PCB_SIZE 132
+.eqv two_PCB_SIZE 264
+.eqv three_PCB_SIZE 396
+.eqv four_PCB_SIZE 528
+.eqv MAX_PCB 1320
 
 .text
 main:
 # prepare the structures
 	jal prep_multi
-	la $a0, STRING_main
-	loop:
-		li $v0, 4
-		syscall
-		j loop
-	li $v0, 10
+	li $v0, 4
+	la $a0, test_string
 	syscall
 		
 	
 # newtask (t0)
-	#la $a0, $t0
-	li $a1, 1
+	la $a0, t0
+	#li $a1, 1
 	jal newtask
-	li $v0, 10
-	syscall
 	
 # newtask(t1)	
-	#la $a0,t1
-	li $a1,2
+	la $a0, t1
+	#li $a1,2
 	jal newtask
+	
+
 # newtask(t2)
-	#la $a0,t2
-	li $a2, 3
+	la $a0, t2
+	#li $a2, 3
 	jal newtask
 
 # startmulti() and continue to 
@@ -66,6 +74,59 @@ prep_multi:
 	la $s1, READY_LIST
 	la $s2, LAST_READY
 	
+	#sw $t0, MAIN_PCB # allocates the first PCB for the main task
+	sw $t0, RUNNING_LIST # running process now points to the main task's PCB
+	move $s0, $t0 # fix for new_task bug
+	
+	addi $t0, $t0, PCB_SIZE
+	sw $t0, AVAILABLE_LIST
+	
+	li $s3, 1 # allocated PCB counter
+	li $s4, 0 # process_id
+	
+	sw $s4, PCB_BLOCKS+76 # stores process id on the main task PCB
+	
+	jr $ra
+	
+newtask:
+	bge $s3, 10, not_enough_space
+	
+	lw $t0, AVAILABLE_LIST
+	bne $s3, 1, keep_going
+	empty_list:
+		move $s1, $t0 # ready = available
+	keep_going:
+		move $s2, $t0 # last ready = available
+		addi $t0, $t0, PCB_SIZE
+		sw $t0, AVAILABLE_LIST # available += PCB_SIZE
+		li $v0, 0
+		b done
+	
+	not_enough_space:
+		li $v0, 1
+	done:
+		#li $t0
+		addi $s3, $s3, 1 # increments the allocated PCB counter
+		move $t0, $s3
+		addi $t0, $t0, -1
+		mulu $t1, $t0, PCB_SIZE
+		addi $t1, $t1, 76
+		la $t2, PCB_BLOCKS
+		add $t2, $t2, $t1
+		sw $t0, 0($t2) # stores the new process id on the correspondent PCB
+		
+		sub $t2, $t2, $t1
+		addi $t2, $t2, 124
+		sw $a0, 0($t2)
+		
+		
+		la $a0, t0
+		li $v0, 4
+		syscall
+		
+		jr $ra
+    
+start_multi:
 	addi $sp, $sp, -4
 	sw $ra, 0($sp)
 	
@@ -74,14 +135,6 @@ prep_multi:
 	lw $ra, 0($sp)
 	addi $sp, $sp, 4
 	
-	jr $ra
-	
-newtask:
-	# write your code here
-	jr $ra
-    
-start_multi:
-	# write your code here
 	jr $ra 
 
 .include "interrupt.asm"
